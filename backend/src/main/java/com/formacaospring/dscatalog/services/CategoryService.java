055a8591bef0f57e4caed243c0756d1240dd2e1f
DyhegoS <dyhegodasilva@gmail.com>
2025-11-13 12:50:04 -0300

    implementado find category by id e tratamento de exceções

--- backend/src/main/java/com/formacaospring/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/formacaospring/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.formacaospring.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.formacaospring.dscatalog.dto.CategoryDTO;
 import com.formacaospring.dscatalog.entities.Category;
 import com.formacaospring.dscatalog.repositories.CategoryRepository;
+import com.formacaospring.dscatalog.services.exceptions.EntityNotFoundException;
+
+
 
 @Service
 public class CategoryService {
@@ -21,5 +25,12 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Recurso não encontrado!"));
+		return new CategoryDTO(entity);
 	}
 }
---
+++ backend/src/main/java/com/formacaospring/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.formacaospring.dscatalog.services.exceptions;
+
+@SuppressWarnings("serial")
+public class EntityNotFoundException extends RuntimeException{
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}	
