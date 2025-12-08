63112d41567d325f25e39648bdbfd6665a250ecf
DyhegoS <dyhegodasilva@gmail.com>
2025-12-08 17:36:33 -0300

    iniciando implementação de segurança

--- backend/src/main/java/com/formacaospring/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/formacaospring/dscatalog/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
-	@SuppressWarnings({ "removal", "deprecation" })
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
--- backend/src/main/java/com/formacaospring/dscatalog/entities/User.java
+++ backend/src/main/java/com/formacaospring/dscatalog/entities/User.java
@@ -86,6 +86,20 @@ public class User {
 		this.password = password;
 	}
 	
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public Set<Role> getRoles() {
 		return roles;
 	}
