8398eddfccc0f5b04da1328da11735b0a3a08e80
DyhegoS <dyhegodasilva@gmail.com>
2025-12-12 16:41:39 -0300

    ajustado o PasswordEncoder na classe AuthorizationServerConfig e criado a classe AppConfig com o PasswordEncoder

--- backend/src/main/java/com/formacaospring/dscatalog/services/UserService.java
+++ backend/src/main/java/com/formacaospring/dscatalog/services/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,7 +32,8 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 public class UserService implements UserDetailsService{
 	
-	private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository repository;
