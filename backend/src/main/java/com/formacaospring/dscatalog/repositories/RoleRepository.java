2bd9d5136ba4d32dd2f82424b59fb0da73aa756e
DyhegoS <dyhegodasilva@gmail.com>
2025-12-23 13:48:12 -0300

    teste em modo de dev com postgres e iniciado implementação de signup

--- backend/src/main/java/com/formacaospring/dscatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/formacaospring/dscatalog/repositories/RoleRepository.java
@@ -7,5 +7,5 @@ import com.formacaospring.dscatalog.entities.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    Role findByAuthority(String authority);
 }
